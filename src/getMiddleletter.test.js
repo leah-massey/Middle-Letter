c4fcfaabf1553f63c6c0382e87c7094a96f2ebb3
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-10-20 15:49:22 +0100

    test: returns a single letter for a word with odd number of letters passed using hahard coding

--- src/getMiddleletter.test.js
+++ src/getMiddleletter.test.js
@@ -1,7 +1,11 @@
 const getMiddleLetter = require("./getMiddleLetter");
 
 describe("getMiddleLetter", () => {
-  it("returns a string", () => {
+  it("returns a string ", () => {
     expect(typeof getMiddleLetter("bat")).toBe("string");
+  });
+
+  it("returns a single letter for a word with odd number of letters", () => {
+    expect(getMiddleLetter("bat")).toBe("a");
   });
 });
